636a98f5030b82081559f8d540a631b80e4e9877
Mateus Faissal <mateusfaissal@gmail.com>
2023-08-26 18:53:27 -0300

    third commit: database connected and project architecture built

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import router from "../src/routes/index.routes.js"
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+
+const server = express();     
+server.use(cors());            
+server.use(express.json());    
+server.use(router)
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+   
+    await mongoClient.connect()
+    console.log("MongoDB connected!")
+} catch (err) {
+    console.log(err.message)
+}
+
+export const db = mongoClient.db()
+
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
