2448f2dcae7c46e5d7e40a9802dbea590997c296
Mateus Faissal <mateusfaissal@gmail.com>
2023-08-28 11:46:45 -0300

    seventh commit: validation type error fixed

--- src/schemas/transactions.schemas.js
+++ src/schemas/transactions.schemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const transactionsSchema = Joi.object({
 
-    type: Joi.string().required(),
+    type: Joi.string().valid("entrada", "saida").required(),
     value: Joi.number().positive().required(),
     description: Joi.string().required()
 
